d30badc4c87e7b30fbc9b8993a3bbc8efae5c697
Jean Menye <jean.menye@sprint-pay.com>
2019-04-26 11:30:04 +0100

    Hello Jenkins World

---
+++ src/main/java/com/sprintpay/funjenkins/App.java
@@ -0,0 +1,13 @@
+package com.sprintpay.funjenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
